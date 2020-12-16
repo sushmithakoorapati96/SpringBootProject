0b3db09a6bcfa0473626efeaa4d63fc2fce490c5
Sushmitha Koorapati <sushmithakoorapati2129@gmail.com>
2020-12-16 12:13:08 +0530

    Spring Application

---
+++ src/main/java/SB/SpringBootProject/App.java
@@ -0,0 +1,13 @@
+package SB.SpringBootProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
